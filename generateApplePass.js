3a76289fdda934cb190c998ae36c92469c8a2b31
innanineelesh <neelesh.innani@gmail.com>
2024-07-25 12:02:02 +0530

    Update generateApplePass.js

--- generateApplePass.js
+++ generateApplePass.js
@@ -13,7 +13,7 @@ const certFile = path.join(certsDir, 'signingCert.pem');
 const keyFile = path.join(certsDir, 'signingKey.pem');
 const wwdrFile = path.join(certsDir, 'WWDR.pem');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     const { studentId, studentName, admissionNo, studentClass, yearGroup, parentId, parentName, parentNumber } = req.body;
 
     const currentDate = new Date().toISOString();
@@ -33,82 +33,90 @@ module.exports = (req, res) => {
 
     jsonfile.writeFileSync(passJsonPath, passJson, { spaces: 2 });
 
-    createPassPackage(res);
+    try {
+        await createPassPackage();
+
+        // Send the JSON response with studentId and token
+        res.json({ studentId, token });
+
+        // Send the pkpass file for download
+        res.download(path.join(__dirname, passFileName), 'StudentPass.pkpass');
+    } catch (err) {
+        console.error('Error generating pass package:', err);
+        res.status(500).send('Error generating pass');
+    }
 };
 
-const createPassPackage = async (res) => {
-    try {
-        const files = fs.readdirSync(passDir);
-        const manifest = {};
+const createPassPackage = async () => {
+    const files = fs.readdirSync(passDir);
+    const manifest = {};
 
-        files.forEach(file => {
-            if (file !== 'manifest.json' && file !== 'signature') {
-                const filePath = path.join(passDir, file);
-                const fileBuffer = fs.readFileSync(filePath);
-                const hash = crypto.createHash('sha1').update(fileBuffer).digest('hex');
-                manifest[file] = hash;
+    files.forEach(file => {
+        if (file !== 'manifest.json' && file !== 'signature') {
+            const filePath = path.join(passDir, file);
+            const fileBuffer = fs.readFileSync(filePath);
+            const hash = crypto.createHash('sha1').update(fileBuffer).digest('hex');
+            manifest[file] = hash;
+        }
+    });
+
+    jsonfile.writeFileSync(path.join(passDir, 'manifest.json'), manifest, { spaces: 2 });
+
+    const manifestPath = path.join(passDir, 'manifest.json');
+    const signaturePath = path.join(passDir, 'signature');
+
+    const privateKeyPem = fs.readFileSync(keyFile, 'utf8');
+    const certPem = fs.readFileSync(certFile, 'utf8');
+    const caCertPem = fs.readFileSync(wwdrFile, 'utf8');
+
+    const pki = forge.pki;
+    const privateKey = pki.privateKeyFromPem(privateKeyPem);
+    const certificate = pki.certificateFromPem(certPem);
+    const caCert = pki.certificateFromPem(caCertPem);
+
+    const p7 = forge.pkcs7.createSignedData();
+    p7.content = forge.util.createBuffer(fs.readFileSync(manifestPath), 'utf8');
+
+    p7.addCertificate(certificate);
+    p7.addCertificate(caCert);
+    p7.addSigner({
+        key: privateKey,
+        certificate: certificate,
+        digestAlgorithm: forge.pki.oids.sha1,
+        authenticatedAttributes: [
+            {
+                type: forge.pki.oids.contentType,
+                value: forge.pki.oids.data
+            },
+            {
+                type: forge.pki.oids.messageDigest
+            },
+            {
+                type: forge.pki.oids.signingTime,
+                value: new Date()
             }
-        });
+        ]
+    });
 
-        jsonfile.writeFileSync(path.join(passDir, 'manifest.json'), manifest, { spaces: 2 });
+    p7.sign();
 
-        const manifestPath = path.join(passDir, 'manifest.json');
-        const signaturePath = path.join(passDir, 'signature');
+    const derBuffer = Buffer.from(forge.asn1.toDer(p7.toAsn1()).getBytes(), 'binary');
+    fs.writeFileSync(signaturePath, derBuffer);
 
-        const privateKeyPem = fs.readFileSync(keyFile, 'utf8');
-        const certPem = fs.readFileSync(certFile, 'utf8');
-        const caCertPem = fs.readFileSync(wwdrFile, 'utf8');
+    const output = fs.createWriteStream(path.join(__dirname, passFileName));
+    const archive = archiver('zip', { zlib: { level: 9 } });
 
-        const pki = forge.pki;
-        const privateKey = pki.privateKeyFromPem(privateKeyPem);
-        const certificate = pki.certificateFromPem(certPem);
-        const caCert = pki.certificateFromPem(caCertPem);
-
-        const p7 = forge.pkcs7.createSignedData();
-        p7.content = forge.util.createBuffer(fs.readFileSync(manifestPath), 'utf8');
-
-        p7.addCertificate(certificate);
-        p7.addCertificate(caCert);
-        p7.addSigner({
-            key: privateKey,
-            certificate: certificate,
-            digestAlgorithm: forge.pki.oids.sha1,
-            authenticatedAttributes: [
-                {
-                    type: forge.pki.oids.contentType,
-                    value: forge.pki.oids.data
-                },
-                {
-                    type: forge.pki.oids.messageDigest
-                },
-                {
-                    type: forge.pki.oids.signingTime,
-                    value: new Date()
-                }
-            ]
-        });
-
-        p7.sign();
-
-        const derBuffer = Buffer.from(forge.asn1.toDer(p7.toAsn1()).getBytes(), 'binary');
-        fs.writeFileSync(signaturePath, derBuffer);
-
-        const output = fs.createWriteStream(path.join(__dirname, passFileName));
-        const archive = archiver('zip', { zlib: { level: 9 } });
-
+    return new Promise((resolve, reject) => {
         output.on('close', () => {
-            res.download(path.join(__dirname, passFileName), 'StudentPass.pkpass');
+            resolve();
         });
 
         archive.on('error', err => {
-            throw err;
+            reject(err);
         });
 
         archive.pipe(output);
         archive.directory(passDir + '/', false);
         archive.finalize();
-    } catch (err) {
-        console.error('Error creating pass package:', err);
-        res.status(500).send('Error generating pass');
-    }
+    });
 };
