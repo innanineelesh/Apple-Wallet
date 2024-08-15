ca18cb20ce6ebe385da806b32039b154e0cdf0e6
innanineelesh <neelesh.innani@gmail.com>
2024-08-15 08:01:29 +0530

    Create refreshApplePass.js

---
+++ refreshApplePass.js
@@ -0,0 +1,123 @@
+const fs = require('fs');
+const path = require('path');
+const jsonfile = require('jsonfile');
+const crypto = require('crypto');
+const forge = require('node-forge');
+const archiver = require('archiver');
+
+const passDir = path.join(__dirname, 'lollipop.pass');
+const certsDir = path.join(__dirname, 'certs');
+const passFileName = 'Lollipop.pkpass';
+
+const certFile = path.join(certsDir, 'signingCert.pem');
+const keyFile = path.join(certsDir, 'signingKey.pem');
+const wwdrFile = path.join(certsDir, 'WWDR.pem');
+
+module.exports = async (req, res) => {
+    const { studentId } = req.body;
+
+    // Generate the new token based on the current date and studentId
+    const currentDate = new Date().toISOString();
+    const newToken = `${currentDate}-${studentId}`;
+
+    const passJsonPath = path.join(passDir, 'pass.json');
+    let passJson;
+    try {
+        passJson = jsonfile.readFileSync(passJsonPath);
+    } catch (err) {
+        console.error('Error reading pass JSON:', err);
+        return res.status(500).send('Error reading pass JSON');
+    }
+
+    // Update the token in the pass JSON
+    const passData = JSON.parse(passJson.barcode.message);
+    passData.token = newToken; // Update the token field
+    passJson.barcode.message = JSON.stringify(passData);
+
+    jsonfile.writeFileSync(passJsonPath, passJson, { spaces: 2 });
+
+    try {
+        await createPassPackage();
+
+        // Serve the updated pass to the user
+        res.download(path.join(__dirname, passFileName), 'UpdatedStudentPass.pkpass');
+    } catch (err) {
+        console.error('Error refreshing pass package:', err);
+        res.status(500).send('Error refreshing pass');
+    }
+};
+
+const createPassPackage = async () => {
+    const files = fs.readdirSync(passDir);
+    const manifest = {};
+
+    files.forEach(file => {
+        if (file !== 'manifest.json' && file !== 'signature') {
+            const filePath = path.join(passDir, file);
+            const fileBuffer = fs.readFileSync(filePath);
+            const hash = crypto.createHash('sha1').update(fileBuffer).digest('hex');
+            manifest[file] = hash;
+        }
+    });
+
+    jsonfile.writeFileSync(path.join(passDir, 'manifest.json'), manifest, { spaces: 2 });
+
+    const manifestPath = path.join(passDir, 'manifest.json');
+    const signaturePath = path.join(passDir, 'signature');
+
+    const privateKeyPem = fs.readFileSync(keyFile, 'utf8');
+    const certPem = fs.readFileSync(certFile, 'utf8');
+    const caCertPem = fs.readFileSync(wwdrFile, 'utf8');
+
+    const pki = forge.pki;
+    const privateKey = pki.privateKeyFromPem(privateKeyPem);
+    const certificate = pki.certificateFromPem(certPem);
+    const caCert = pki.certificateFromPem(caCertPem);
+
+    const p7 = forge.pkcs7.createSignedData();
+    p7.content = forge.util.createBuffer(fs.readFileSync(manifestPath), 'utf8');
+
+    p7.addCertificate(certificate);
+    p7.addCertificate(caCert);
+    p7.addSigner({
+        key: privateKey,
+        certificate: certificate,
+        digestAlgorithm: forge.pki.oids.sha1,
+        authenticatedAttributes: [
+            {
+                type: forge.pki.oids.contentType,
+                value: forge.pki.oids.data
+            },
+            {
+                type: forge.pki.oids.messageDigest
+            },
+            {
+                type: forge.pki.oids.signingTime,
+                value: new Date()
+            }
+        ]
+    });
+
+    p7.sign();
+
+    const derBuffer = Buffer.from(forge.asn1.toDer(p7.toAsn1()).getBytes(), 'binary');
+    fs.writeFileSync(signaturePath, derBuffer);
+
+    const output = fs.createWriteStream(path.join(__dirname, passFileName));
+    const archive = archiver('zip', { zlib: { level: 9 } });
+
+    return new Promise((resolve, reject) => {
+        output.on('close', () => {
+            console.log(`Created pass package with ${archive.pointer()} total bytes`);
+            resolve();
+        });
+
+        archive.on('error', err => {
+            reject(err);
+        });
+
+        archive.pipe(output);
+        archive.directory(passDir + '/', false);
+        archive.finalize();
+    });
+};
