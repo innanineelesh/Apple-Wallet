9ddcdfb737cbfdecfda71ac96eb250a65badab0f
innanineelesh <neelesh.innani@gmail.com>
2024-10-01 21:38:29 +0530

    Update passes.js

--- passes.js
+++ passes.js
@@ -1,1 +1,12 @@
-console.log("Inside Passes");
+// passes.js
+const express = require('express');
+const router = express.Router();
+
+// Device registration endpoint
+router.post('/', (req, res) => {
+    console.log('Device Registration Request Received'); // Log statement
+
+    res.sendStatus(200); // Respond with a 200 status code
+});
+
+module.exports = router; // Export the router
